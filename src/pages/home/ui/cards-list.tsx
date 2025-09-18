8bc9a7fad69b1101d9ce88f84e8473a0c0e7dcba
Daniil Khudyakov <daniil.khudyakov@travelline.ru>
2025-09-18 17:45:57 +0300

    Added masonry

--- src/pages/home/ui/cards-list.tsx
+++ src/pages/home/ui/cards-list.tsx
@@ -2,11 +2,46 @@
 
 import { CardItem } from './card-item';
 
+import { Masonry } from '@/shared/ui/masonry';
 import { useCardStore } from '@/entities/card';
 
 type CardsListProps = {
     selectedTagIds: string[];
 };
+
+const items = [
+    {
+        id: '1',
+
+        img: 'https://picsum.photos/id/1015/600/900?grayscale',
+
+        url: 'https://example.com/one',
+
+        height: 400,
+    },
+
+    {
+        id: '2',
+
+        img: 'https://picsum.photos/id/1011/600/750?grayscale',
+
+        url: 'https://example.com/two',
+
+        height: 250,
+    },
+
+    {
+        id: '3',
+
+        img: 'https://picsum.photos/id/1020/600/800?grayscale',
+
+        url: 'https://example.com/three',
+
+        height: 600,
+    },
+
+    // ... more items
+];
 
 export function CardsList({ selectedTagIds }: CardsListProps) {
     const { cards } = useCardStore();
@@ -21,6 +56,17 @@ export function CardsList({ selectedTagIds }: CardsListProps) {
             {filteredCards.map((card) => (
                 <CardItem key={card.id} card={card} />
             ))}
+            <Masonry
+                animateFrom="bottom"
+                blurToFocus={true}
+                colorShiftOnHover={false}
+                duration={0.6}
+                ease="power3.out"
+                hoverScale={0.95}
+                items={items}
+                scaleOnHover={true}
+                stagger={0.05}
+            />
         </div>
     );
 }
