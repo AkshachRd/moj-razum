c302b57432c8ed525c2971f44e64613cebbd9210
Daniil Khudyakov <daniil.khudyakov@travelline.ru>
2025-09-18 15:56:17 +0300

    init tauri

--- next.config.js
+++ next.config.js
@@ -1,10 +1,24 @@
+const isProd = process.env.NODE_ENV === 'production';
+
+const internalHost = process.env.TAURI_DEV_HOST || 'localhost';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // Ensure Next.js uses SSG instead of SSR
+    // https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
+    output: 'export',
+    // Note: This feature is required to use the Next.js Image component in SSG mode.
+    // See https://nextjs.org/docs/messages/export-image-api for different workarounds.
+    images: {
+        unoptimized: true,
+    },
+    // Configure assetPrefix or else the server won't properly resolve your assets.
+    assetPrefix: isProd ? undefined : `http://${internalHost}:4250`,
     experimental: {
         serverActions: {
             allowedOrigins: [
-                'localhost:3000', // localhost
-                'super-cod-3000.app.github.dev', // Codespaces
+                'localhost:4250', // localhost
+                'super-cod-4250.app.github.dev', // Codespaces
             ],
         },
     },
